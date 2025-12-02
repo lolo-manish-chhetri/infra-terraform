1bd5df5053327a27dfc52ff17bbb94a7e7a5bebd
lolo-manish-chhetri <ext.manish.chhetri@lololegacy.com>
2025-12-02 11:20:54 +0000

    Rename Cloud Run service to "cloud-run-learning-prod-v1" in main.tf

--- envs/prod/main.tf
+++ envs/prod/main.tf
@@ -14,7 +14,7 @@ provider "google" {
 
 
 resource "google_cloud_run_service" "cloud-run" {
-  name     = "cloud-run-learning-prod"
+  name     = "cloud-run-learning-prod-v1"
   location = "us-central1"
   project  = "rich-torus-428117-m8"
   template {
