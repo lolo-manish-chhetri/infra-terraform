ad8d209eca251539eb2d62b6fd03982dec3b831c
lolo-manish-chhetri <ext.manish.chhetri@lololegacy.com>
2025-12-02 09:44:06 +0000

    Add Terraform workflows for apply, destroy, and PR plan; update .gitignore and add Cloud Run resource

---
+++ envs/prod/main.tf
@@ -0,0 +1,16 @@
+resource "google_cloud_run_service" "cloud-run" {
+  name     = "cloud-run-prod"
+  location = "us-central1"
+  project  = "rich-torus-428117-m8"
+  template {
+    spec {
+      containers {
+        image = "gcr.io/cloud-run/hello"
+      }
+    }
+  }
+  traffic {
+    percent = 100
+    latest_revision = true
+  }
+}
